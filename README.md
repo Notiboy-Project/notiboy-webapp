84e69e28ee7fdae52b3646a7e14b118dd008ebe6
Vidhyanand CS <vithushaji@gmail.com>
2022-11-07 14:50:13 +0530

    updating ui for bulk sending

--- README.md
+++ README.md
@@ -18,8 +18,4 @@ npm run build
 ### Lints and fixes files
 ```
 npm run lint
-```
-
-### SDK integrated with UI
-As of now the SDK is integrated with UI and will be published as a separate package in the future.
-You can check the SDK at `src/sdk` 
+``` 
--- src/sections/SendNotification.vue
+++ src/sections/SendNotification.vue
@@ -11,6 +11,10 @@
       </option>
     </select>
     <div class="channel-type">
+      <div class="channel-type-public">
+        <input type="radio" id="public" value="public" v-model="channelType" />
+        <label for="public">Public</label>
+      </div>
       <div class="channel-type-private">
         <input
           type="radio"
@@ -19,12 +23,13 @@
           v-model="channelType"
         />
         <label for="personal">Personal</label>
-      </div>
-      <div class="channel-type-public">
-        <input type="radio" id="public" value="public" v-model="channelType" />
-        <label for="public">Public</label>
+      </div>   
+      <div class="channel-type-bulk">
+        <input type="radio" id="bulk" value="bulk" v-model="channelType" />
+        <label for="bulk">Bulk Personal</label>
       </div>
     </div>
+    <Transition>
     <input
       id="receiverAddress"
       v-if="channelType == 'personal'"
@@ -33,20 +38,31 @@
       placeholder="Please Input Receiver Address"
       label="userAddress"
     />
-
+    </Transition>
+    <Transition>
     <textarea
       v-model="notification"
+      v-if="channelType == 'personal'|| channelType == 'public'"
       placeholder="Please enter the notification"
       style="resize: none"
       id="w3review"
       name="w3review"
     ></textarea>
-    <button @click.prevent="sendNotification">Send Notification</button>
+    </Transition>
+    <div class="send-buttons">
+      <div v-if="channelType == 'bulk'" class="send-buttons_upload">
+        <input type="file" id="fileUpload" @change="extractDataCsv($event)" hidden/>
+        <label for="fileUpload" type="button" class="upload-button">Upload</label>
+        <span>{{csvFileName}}</span>
+      </div>
+      <button @click.prevent="sendNotification">Send Notification</button>
+    </div>
   </div>
 </template>
 <script>
 import { mapGetters } from "vuex";
 import store from "../store";
+import Papa from 'papaparse';
 export default {
   data() {
     return {
@@ -55,6 +71,11 @@ export default {
       notification: "",
       filters: [],
       receiverAddress: "",
+      bulkImport:"",
+      csvFileName:"None",
+      csvFile:"",
+      csvFileContent: [],
+      parsed: false
     };
   },
 
@@ -82,22 +103,50 @@ export default {
           channelName: this.selectedChannel,
           notification: this.notification,
         });
+      }else if (this.channelType == "bulk"){
+        store.dispatch("sendPersonalNotification", {
+          address: this.userAddress,
+          receiverAddress: this.receiverAddress,
+          channelName: this.selectedChannel,
+          notification: this.notification,
+        });
       }
     },
+    extractDataCsv(event){
+      this.parsed = false;
+      this.csvFileName = event.target.files[0].name;
+      this.csvFile = event.target.files[0];
+      this.parseFile();
+    },
+    parseFile(){
+      Papa.parse( this.csvFile, {
+          header: true,
+          skipEmptyLines: true,
+          complete: function(results){
+              this.csvFileContent = results.data;
+              this.parsed = true;
+          }.bind(this)
+      } );
+    }
   },
-
   created() {
     store.dispatch("getChannelList");
   },
 };
 </script>
 <style scoped>
+:root{
+  --radio: #7C96B2;
+  --radio-checked: #4F29F0;
+  --radio-size: 50px;
+  --background: #ffffff;
+}
 .send-card {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
-  height: 50rem;
+  height: 60rem;
   padding-top: 8rem;
   padding-bottom: 5rem;
   font-weight: bold;
@@ -129,7 +178,11 @@ select option {
   justify-content: space-between;
 }
 
-.channel-type-public {
+.channel-type-private {
+  padding-left: 3rem;
+}
+
+.channel-type-bulk{
   padding-left: 3rem;
 }
 
@@ -184,8 +237,14 @@ select:focus {
   -moz-box-shadow: none;
   -webkit-box-shadow: none;
 }
-
-button {
+.send-buttons{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  gap: 60px;
+}
+button, .upload-button {
   background: var(--teritary);
   border: var(--teritary);
   padding: 1rem;
@@ -193,5 +252,107 @@ button {
   color: white;
   font-weight: bold;
   cursor: pointer;
+}
+.send-buttons_upload{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 5px;
+}
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.25s ease;
+}
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+input[type=radio] {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  background: var(--primary);
+  position: relative;
+  height: 1.75em;
+  width: 1.75em;
+  outline: none;
+  margin-right: 0.5em;
+  cursor: pointer;
+  border: 2px solid var(--teritary);
+  background: transparent;
+  border-radius: 50%;
+  overflow: hidden;
+  transition: border 0.5s ease;
+  transform: translateY(0.5em);
+}
+input[type=radio]::before,input[type=radio]::after {
+  content: "";
+  display: flex;
+  justify-self: center;
+  border-radius: 50%;
+  justify-content: center;
+}
+input[type=radio]::before {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: var(--primary);
+  z-index: 1;
+  opacity: var(--opacity, 1);
+}
+input[type=radio]::after {
+  position: relative;
+  width: calc(100%);
+  height: calc(100%);
+  background: var(--teritary);
+  top: var(--y, 100%);
+  transition: top 0.5s cubic-bezier(0.48, 1.97, 0.5, 0.63);;
+}
+input[type=radio]:checked {
+  --radio: var(--teritary);
+}
+input[type=radio]:checked::after {
+  --y: 0%;
+  -webkit-animation: stretch-animate 0.3s ease-out 0.17s;
+          animation: stretch-animate 0.3s ease-out 0.17s;
+}
+input[type=radio]:checked::before {
+  --opacity: 0;
+}
+input[type=radio]:checked ~ input[type=radio]::after {
+  --y: -100%;
+}
+input[type=radio]:not(:checked)::before {
+  --opacity: 1;
+  transition: opacity 0s linear 0.5s;
+}
+
+@-webkit-keyframes stretch-animate {
+  0% {
+    transform: scale(1, 1);
+  }
+  28% {
+    transform: scale(1.15, 0.85);
+  }
+  50% {
+    transform: scale(0.9, 1.1);
+  }
+  100% {
+    transform: scale(1, 1);
+  }
+}
+
+@keyframes stretch-animate {
+  0% {
+    transform: scale(1, 1);
+  }
+  28% {
+    transform: scale(1.15, 0.85);
+  }
+  50% {
+    transform: scale(0.9, 1.1);
+  }
+  100% {
+    transform: scale(1, 1);
+  }
 }
 </style>
